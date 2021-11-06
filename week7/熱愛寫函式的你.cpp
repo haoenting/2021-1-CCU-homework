272bf3da9e1a9adc7fa59d8a1134eb2a1bf02cf9
haoenting <91521504+haoenting@users.noreply.github.com>
2021-11-06 18:33:19 +0800

    Update 熱愛寫函式的你.cpp

--- week7/熱愛寫函式的你.cpp
+++ week7/熱愛寫函式的你.cpp
@@ -1,27 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<math.h>
-void isPrime(int num){
-  int TF = 1;
-  if(num==2)TF = 1;
-  else if(num%2 == 0|| num == 1)TF = 0;
-  else{
-   for(int x=3;x<=sqrt(num);x+=2){
-     if(num%x==0){
-       TF=0;
-       break;
-     }
-  }
-    if(TF==1)printf("True\n");
+void isPrime(int num) {
+    int TF = 1;
+    if (num == 2)TF = 1;
+    else if (num % 2 == 0 || num == 1)TF = 0;
+    else {
+        for (int x = 3; x <= sqrt(num); x += 2) {
+            if (num % x == 0) {
+                TF = 0;
+                break;
+            }
+        } 
+    }
+    if (TF == 1)printf("True\n");
     else printf("False\n");
 }
-int main(){
-  int count,num;
-  scanf("%d",&count);
-  
-  for(int x=0;x<count;x++){
-    scanf("%d",&num);
-    isPrime(num);
-  }
-  return 0;
+int main() {
+    int count;
+    long long int num;
+    scanf("%d", &count);
+    for (int x = 0; x < count; x++) {
+        scanf("%ld", &num);
+        printf("%d\n", num);
+        isPrime(num);
+    }
+    system("pause");
+    return 0;
 }
