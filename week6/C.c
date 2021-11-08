90290d9189026cca74bf1cb4ce39bb10bce009a8
haoenting <91521504+haoenting@users.noreply.github.com>
2021-11-08 09:53:22 +0800

    Update C.c

--- week6/C.c
+++ week6/C.c
@@ -1,26 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int pri[1000000000];
+#include<math.h>
 void isPrime(int num) {
-	if (pri[num] == 0)printf("False\n");
-	else printf("True\n");
+    int TF = 1;
+    if (num == 2)TF = 1;
+    else if (num % 2 == 0 || num == 1)TF = 0;
+    else {
+        for (int x = 3; x <= sqrt(num); x += 2) {
+            if (num % x == 0) {
+                TF = 0;
+                break;
+            }
+        } 
+    }
+    if (TF == 1)printf("True\n");
+    else printf("False\n");
 }
 int main() {
-	int n, num;
-	pri[1] = 0;
-	for (int x = 4; x <= 1000000000; x++) {
-		for (int y = 2; y < x / 2; y++) {
-			if (x % y == 0) {
-				pri[x] = 0;
-				break;
-			}
-		}
-	}
-	scanf("%d", &n);
-	for (int x = 0; x < n; x++) {
-		scanf("%d", &num);
-		isPrime(num);
-	}
-	system("pause");
-	return 0;
+    int count;
+    long long int num;
+    scanf("%d", &count);
+    for (int x = 0; x < count; x++) {
+        scanf("%ld", &num);
+        printf("%d\n", num);
+        isPrime(num);
+    }
+    system("pause");
+    return 0;
 }
