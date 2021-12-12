3ab3e4587a3d823df1a38a2589c71c6267fb57bd
haoenting <91521504+haoenting@users.noreply.github.com>
2021-12-12 15:54:33 +0800

    Update C.c

--- week11/C.c
+++ week11/C.c
@@ -3,44 +3,34 @@
 #include<stdlib.h>
 #include<string.h>
 int main() {
-    char k[1005];
+    int num = 3;
+    char k[1005], m[1005];
     unsigned long long int t;
     scanf("%s", k);
-    while (scanf("%llu", &t) != EOF){
-        char m[1005];
-        int num[20],a=0,b=0; 
-        while (t != 0) {
-           num[a] = t % 256 ;
-           m[a] = k[b] ^ num[a];
-           t /= 256;
-           a++;
-           b++;
-           if (k[b] == NULL)b = 0;
+    while (scanf("%llu", &t) != EOF) {
+        for (int a = 0 ; a < 4 ; a++, t /= 256, num--) {
+            m[num] = t % 256 ;
         }
-        a--;
-        b = 0;
-        int count = 0;
-        while (a >= 0) {
-           m[a] ^= k[b];
-            printf("%c",m[a]);
-            b++;
-            a--;
-            if (k[b] == NULL)b = 0;
-            count++;
-        }
-        while ((count-1) % 4 == 0) {
-            printf("x");
-            count++;
-        }
+        num += 8;
     }
-    // a b c d e f a b kbar
-    // b a f e d c b a k
-    // 7 6 5 4 3 2 1 0 num
+    num -= 4;
+    for (int b = 0,c = num; c >= 0;b++,c--) {      
+        if (k[b] == NULL)b = 0;
+        m[c] ^= k[b];
+    }
+    for (int b = 0,c = 0; c <= num; b++,c++) {
+       if (k[b] == NULL)b = 0;
+       m[c]^=k[b];
+       printf("%c", m[c]);
+    }
     //system("pause");
     return 0;
 }
+//a b c d  e a b c kbar
+//c b a e  d c b a  k
+//7 6 5 4  3 2 1 0  num
 /*
-hate 1146714747 1749842552 147 152
+hate 1146714747 1749842552
 
 ILoveYou
 */
